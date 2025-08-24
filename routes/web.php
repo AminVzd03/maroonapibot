6bac3d9db579b0ad38575113e770972a2559a6ce
Amin Valizade <aminvalizade20@gmail.com>
2025-08-24 16:55:03 +0330

    first cmmit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/' . env('TELEGRAM_BOT_TOKEN') . '/webhook', [TelegramController::class, 'webhook']);
+
+Route::get('/welcome',function(){
+    return view('welcome');
+});
